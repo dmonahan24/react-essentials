acc57685faaf1731d0cf548aa5d70f54f1602435
Dave Monahan <dmonahan24@gmail.com>
2021-03-04 08:50:55 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import orangeSUP from "./img/orange-paddleboard.jpg";
+
+function Header(props) {
+  console.log(props);
+  return (
+    <header>
+      <h1>Sam's Stand-up Shop</h1>
+    </header>
+  )
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>
+        Sam's Stand-up Shop has everything you need for paddle boarding.
+        We've got the {props.adjective} paddle boards you'll find and high quality, 
+        long-lasting inflator pumps.
+      </p>
+      <img src={orangeSUP}
+      height={240}
+      alt='SUP on the beach' 
+      />
+      <ul>
+        {props.paddleTypes.map((paddle) => (
+        <li key={paddle.id}>{paddle.title}</li>
+        ))}
+      </ul>
+    </section>
+
+  )
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p> Contact Us - Local Pick Up - Our Company</p>
+      <span>{props.year}</span>
+    </footer>
+  )
+}
+
+const paddleTypes = [
+  "Wood",
+  "Plastic/ABS",
+  "Carbon Fiber"
+];
+
+const paddleObjects = paddleTypes.map((paddle, i) => ({id: i, title:paddle}))
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name='head'/>
+      <Main adjective='best' paddleTypes={paddleObjects} />
+      <Footer year={new Date().getFullYear()} />
+      
     </div>
   );
 }
