85fde6a90b9520210a1ad0a27ea21890dabdf0bb
Dave Monahan <dmonahan24@gmail.com>
2021-03-04 14:19:00 -0500

    React.js essntial training course complete

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
-import React, { useState, useEffect } from 'react';
 import './App.css';
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import {
+  Home, 
+  About, 
+  Services, 
+  CompanyHistory, 
+  Location,  
+  Events, 
+  Contact, 
+  Whoops404} from './pages';
 
-// https://api.github.com/users/dmonahan24
+function App() {
 
-function App({ login }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if(!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-    .then(response => response.json())
-    .then(setData)
-    .then(() => setLoading(false))
-    .catch(setError);
-  }, [login]);
-
-  if (loading) return <h1>Loading...</h1>
-
-  if(data) {
     return (
       <div>
-        <h1>{data.login}</h1>
-        <p>{data.location}</p>
-        <img alt={data.login} src={data.avatar_url} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />}>
+            <Route path='/services' element={<Services />} />
+            <Route path='/history' element={<CompanyHistory />} />
+            <Route path='/location' element={<Location />} />
+          </Route>
+          <Route path='/events' element={<Events />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='*' element={<Whoops404 />} />
+        </Routes>
       </div>
-    )
-  }
-  return (
-    <div>
-      No User Available
-    </div>
-  );
-
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from "react-router-dom"
 
 
 ReactDOM.render(
-    <App login="dmonahan24"/>,
-
+  <Router>
+    <App login="dmonahan24"/>
+  </Router>,
   document.getElementById('root')
 );
 
